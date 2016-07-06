8fd5f62a57e9bd45930f9a0f13ea164b2e720886
Billiam <billiamthesecond@gmail.com>
2016-07-06 00:40:45 -0500

    Adding user model.

---
+++ lib/giftrap/repo.ex
@@ -0,0 +1,3 @@
+defmodule Giftrap.Repo do
+  use Ecto.Repo, otp_app: :giftrap
+end
---
+++ web/models/user.ex
@@ -0,0 +1,21 @@
+defmodule Giftrap.User do
+  use Giftrap.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :email, :password, :password_hash])
+    |> validate_required([:name, :email, :password, :password_hash])
+  end
+end

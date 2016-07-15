36290112ce1fada08b680a9a4be4494ef7817ebb
Billiam <billiamthesecond@gmail.com>
2016-07-14 21:58:10 -0500

    Adding production configuration.

--- config/prod.exs
+++ config/prod.exs
@@ -13,12 +13,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :giftrap, Giftrap.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: System.get_env("APP_HOST"), port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :giftrap, Giftrap.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+  
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
@@ -62,4 +69,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
